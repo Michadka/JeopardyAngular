792769ec4efe210215ae0767675bab029d351adc
Michael Standarski <michael.standarski@libertymutual.com>
2017-07-12 16:46:36 -0400

    initial commit

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -21,30 +21,6 @@ export class ScoreComponent implements OnInit {
     }
     this.scored.emit();
   }
-
-
-// $('#submit-button').click(function(event) {
-//     if (getText() == answer) {
-//       console.log("YAY");
-//       score_amount.html(parseInt(points) + parseInt(score_amount.html()));
-//       newQuestion();
-//       //console.log(getText());
-//     } else {
-//       console.log("wrong");
-//       score_amount.html(parseInt(score_amount.html() - parseInt(points)));
-//       newQuestion();
-//       //console.log(getText())
-
-//     }
-//     $('#answer-value').val("");
-//   })
-
-//   function getText() {
-//     return $('#answer-value').val();
-//   }
-
-
-
  
 
   constructor() { }
